23629851741c8e1df5247bfe861b95f5be21f6ce
bobpp <bobpp.asroma+github@gmail.com>
2011-09-19 20:22:45 +0900

    make include files (header, footer)

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<audio id="preview"></audio> 
+<div id="search_script" class="search_script"></div>
+
+<script src="/javascripts/jquery.min.js" type="text/javascript"></script>
+<script src="/javascripts/jquery.mobile-1.0b2.min.js" type="text/javascript"></script>
+<script src="/javascripts/const.js" type="text/javascript"></script>
+<script src="/javascripts/global.js" type="text/javascript"></script>
+<script src="/javascripts/callback.js" type="text/javascript"></script>
+<script src="/javascripts/bookmark.js" type="text/javascript"></script>
+<script src="/javascripts/itunes_search.js" type="text/javascript"></script>
+<script src="/javascripts/global_mediator.js" type="text/javascript"></script>
+<script src="/javascripts/search_mediator.js" type="text/javascript"></script>
+<script src="/javascripts/bookmark_mediator.js" type="text/javascript"></script>
+<script src="/javascripts/album_mediator.js" type="text/javascript"></script>
+<script src="/javascripts/list_mediator.js" type="text/javascript"></script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8809253-5']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>iPhoneで音楽試聴 - 30sec.fm</title>
+  <link href="/stylesheets/jquery.mobile-1.0b2.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="/stylesheets/common.css" media="screen" rel="stylesheet" type="text/css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <link rel="apple-touch-icon" href="/images/icon.png" />
+</head>
+<body>
+<div data-role="page" data-theme="a" id="container" class="container">
+  <div data-role="header" data-backbtn="false" id="header">
+    <h1>30sec.fm</h1>
+  </div><!-- #header -->
