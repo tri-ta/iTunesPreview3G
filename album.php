ca67125deac167349d648340cd6feaecd1b52617
bobpp <bobpp.asroma+github@gmail.com>
2011-09-19 20:23:01 +0900

    use include files

--- album.php
+++ album.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>iPhoneで音楽試聴 - 30sec.fm</title>
-  <link href="/stylesheets/jquery.mobile-1.0b2.css" media="screen" rel="stylesheet" type="text/css" />
-  <link href="/stylesheets/common.css" media="screen" rel="stylesheet" type="text/css" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-  <link rel="apple-touch-icon" href="/images/icon.png" />
-</head>
-<body>
-<div data-role="page" data-theme="a" id="container" class="container">
-  <div data-role="header" data-backbtn="false">
-    <h1>30sec.fm</h1>
-  </div><!-- #header -->
+<?php include("includes/header.php"); ?>
+
   <div id="menu">
     <a href="javascript:jump2Search()" id="menu_search" class="menu_search">曲を検索</a>
     <a href="javascript:jump2Bookmarks()" id="menu_bookmark" class="menu_bookmark">お気に入り</a>
@@ -25,30 +12,6 @@
     <p>&copy; <a href="http://tri-ta.com" target="_blank">tri-ta</a>, <a href="http://blk.jp" target="_blank">blk</a></p>
   </div>
 </div><!-- #container -->
-<audio id="preview"></audio> 
-<div id="search_script" class="search_script"></div>
-<script src="/javascripts/jquery.min.js" type="text/javascript"></script>
-<script src="/javascripts/jquery.mobile-1.0b2.min.js" type="text/javascript"></script>
-<script src="/javascripts/const.js" type="text/javascript"></script>
-<script src="/javascripts/global.js" type="text/javascript"></script>
-<script src="/javascripts/callback.js" type="text/javascript"></script>
-<script src="/javascripts/bookmark.js" type="text/javascript"></script>
-<script src="/javascripts/itunes_search.js" type="text/javascript"></script>
-<script src="/javascripts/global_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/search_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/bookmark_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/album_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/list_mediator.js" type="text/javascript"></script>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-8809253-5']);
-  _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-</body>
-</html>
+<?php include("includes/footer.php"); ?>
+
