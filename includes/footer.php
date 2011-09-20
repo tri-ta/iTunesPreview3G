608038f81a3d93d6236ac62c789af2e03bca89bd
bobpp <bobpp.asroma+github@gmail.com>
2011-09-21 00:51:31 +0900

    using minify files

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,33 @@
 <audio id="preview"></audio> 
 <div id="search_script" class="search_script"></div>
 
-<script src="/javascripts/jquery.min.js" type="text/javascript"></script>
-<script src="/javascripts/jquery.mobile-1.0b2.min.js" type="text/javascript"></script>
-<script src="/javascripts/const.js" type="text/javascript"></script>
-<script src="/javascripts/global.js" type="text/javascript"></script>
-<script src="/javascripts/callback.js" type="text/javascript"></script>
-<script src="/javascripts/bookmark.js" type="text/javascript"></script>
-<script src="/javascripts/itunes_search.js" type="text/javascript"></script>
-<script src="/javascripts/global_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/search_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/bookmark_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/album_mediator.js" type="text/javascript"></script>
-<script src="/javascripts/list_mediator.js" type="text/javascript"></script>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-8809253-5']);
-  _gaq.push(['_trackPageview']);
+<?php if ($_SERVER['IS_30SEC_FM_PRODUCTION']) : ?>
+	<script src="/javascripts/min/minify.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-8809253-5']);
+		_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+<?php else: ?>
+	<script src="/javascripts/jquery.min.js" type="text/javascript"></script>
+	<script src="/javascripts/jquery.mobile-1.0b2.min.js" type="text/javascript"></script>
+	<script src="/javascripts/const.js" type="text/javascript"></script>
+	<script src="/javascripts/global.js" type="text/javascript"></script>
+	<script src="/javascripts/callback.js" type="text/javascript"></script>
+	<script src="/javascripts/bookmark.js" type="text/javascript"></script>
+	<script src="/javascripts/itunes_search.js" type="text/javascript"></script>
+	<script src="/javascripts/global_mediator.js" type="text/javascript"></script>
+	<script src="/javascripts/search_mediator.js" type="text/javascript"></script>
+	<script src="/javascripts/bookmark_mediator.js" type="text/javascript"></script>
+	<script src="/javascripts/album_mediator.js" type="text/javascript"></script>
+	<script src="/javascripts/list_mediator.js" type="text/javascript"></script>
+<?php endif; ?>
 </body>
 </html>
+
--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,12 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>iPhoneで音楽試聴 - 30sec.fm</title>
+<?php if ($_SERVER['IS_30SEC_FM_PRODUCTION']) : ?>
+  <link href="/stylesheets/min/minify.css" media="screen" rel="stylesheet" type="text/css" />
+<?php else: ?>
   <link href="/stylesheets/jquery.mobile-1.0b2.css" media="screen" rel="stylesheet" type="text/css" />
   <link href="/stylesheets/common.css" media="screen" rel="stylesheet" type="text/css" />
+<?php endif; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <link rel="apple-touch-icon" href="/images/icon.png" />
 </head>
